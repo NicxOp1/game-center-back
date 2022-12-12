9ba1eb0247a38c0b10a81b99edd4973de2f70ae4
RamiroRojas02 <ramirojasarg@gmail.com>
2022-12-12 13:13:29 -0300

    dotenv

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var createError = require('http-errors');
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./config/database/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
