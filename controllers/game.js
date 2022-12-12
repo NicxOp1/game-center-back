fe9e7177775f12fbdaa021951bb1b9b20d8f3083
RamiroRojas02 <ramirojasarg@gmail.com>
2022-12-12 17:21:19 -0300

    controlador para poder ver los games

--- controllers/game.js
+++ controllers/game.js
@@ -3,7 +3,14 @@ const Game = require('../models/game')
 const controller = {
     read: async (req,res) => {
         try {
-            
+            let games = await Game.find()
+            if (games) {
+                res.status(200).json({
+                    res: games,
+                    success: true,
+                    message: `${games.length} games found`
+                })
+            }
         } catch (error) {
             res.status(404).json({
                 success: false,
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 let router =require('express').Router()
-
+let games = require('./games.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'En expresss' });
 });
-
+router.use('/games',games)
 module.exports = router;
