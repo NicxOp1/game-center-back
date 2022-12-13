a2d0c66205d0e76eea5f3ebb1fb7806a0bd8547f
Aldana Chavez Bravo <114601088+aldanamagalicb@users.noreply.github.com>
2022-12-12 21:20:54 -0300

    fix schema de comments

--- schemas/comment.js
+++ schemas/comment.js
@@ -1,8 +1,7 @@
 const joi = require("joi");
 
 const schema = joi.object({
-    showId: joi.any(),
-    itineraryId: joi.any(),
+    gamed: joi.any(),
     userId: joi.any(),
     comment: joi.string().required().min(3).messages({
         'string.min': 'The comment must have at least 3 characters',
