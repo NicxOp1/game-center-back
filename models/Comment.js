f241dbc3d9b0b7e156932a43983b451c792133a2
Aldana Chavez Bravo <114601088+aldanamagalicb@users.noreply.github.com>
2022-12-14 13:31:43 -0300

    fix responses and models

--- models/Comment.js
+++ models/Comment.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    date: { type: Date, required: true },
-    comment: { type: String, required: true, required: true }, 
-    userId: {type: mongoose.Types.ObjectId, ref: 'users', required: true},  
-    gameId: {type: mongoose.Types.ObjectId,ref: 'games'},  
-})
+    comment: { type: String, required: true },
+    userId: { type: mongoose.Types.ObjectId, ref: 'users' },
+    gameId: { type: mongoose.Types.ObjectId, ref: 'games' },
+},
+    {
+        timestamps: true
+    }
+)
 
 
 const Comment = mongoose.model('comments', schema);
