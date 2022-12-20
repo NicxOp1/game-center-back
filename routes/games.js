35c5fca799e9b655e93a788c13efb8624389c66a
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-12-20 18:26:24 -0300

    controlador y ruta para games details

--- routes/games.js
+++ routes/games.js
@@ -1,8 +1,9 @@
 let router=require('express').Router()
-let {read,create,update, destroy} = require('../controllers/game')
+let {read,create,update, destroy,readDetails} = require('../controllers/game')
 const passport = require ('../config/passport')
 
 router.get('/',read)
+router.get('/:id',readDetails)
 router.post('/', create,passport.authenticate("jwt", { session: false }))
 router.put('/:id',update,passport.authenticate("jwt", { session: false }))
 router.delete('/:id',destroy,passport.authenticate("jwt", { session: false }))
